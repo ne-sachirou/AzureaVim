ccdb78c6af7e3862c4d44204289c3cce994f0b2c
ne-sachirou <utakata.c4se@gmail.com>
2011-03-09 15:01:53 +0900

    :earthquake command integrate.

--- src/plugins/earthquake.js
+++ src/plugins/earthquake.js
@@ -1,10 +1,24 @@
 // ==AzureaScript==
 // @name 地震なう
 // @author http://c4se.sakura.ne.jp/profile/ne.html
-// @date 2011-03-09
+// @date 2011-03-10
 // @scriptUrl https://github.com/ne-sachirou/AzureaVim/raw/master/src/plugins/
 // @license public domain
 // ==/AzureaScript==
+
+try {
+    AzureaUtil.mixin(AzureaVim.commands_list, {
+        earthquake: 'earthquake',
+        e: 'earthquake',
+        jisin: 'earthquake',
+        'え': 'earthquake',
+        'じしん': 'earthquake'
+    });
+} catch (err) {}
+// :earthquake [set option2]
+// チャーハン諸島風地震なう機能です。Ctrl+↓で、設定した文を、時刻付きでpostします。
+// AzureaVim pluginとして丈でなく、単独のAzureaScriptとして使えます（其の場合コマンド機能は使用出来ません）。
+// option1にsetを入れた場合、投稿文を設定します。
 
 
 (function() {
@@ -37,7 +51,7 @@ function setEarthquakeMessage() { // @return String:
 }
 
 
-function postEarthquake() { // @return String:
+function postEarthquakeMessage() { // @return String:
     var text = _getMessage() + ' ' + (new Date()).toString();
     
     TwitterService.status.update(text, 0);
@@ -46,29 +60,23 @@ function postEarthquake() { // @return String:
 
 
 System.addContextMenuHandler('地震なう設定', 0, setEarthquakeMessage);
-System.addKeyBindingHandler(0x28, // VK_DOWN
+System.addKeyBindingHandler(0x28, // VK_DOWN ↓
                             2, // Ctrl
-                            postEarthquake);
-
-
+                            postEarthquakeMessage);
 try {
     AzureaVim.prototype.earthquake = function() { // @return String:
         var result;
         
         switch (this.command[1]) {
         case 'set':
-            result = setEarthquakeMessage();
+            result = _setMessage(this.command[2]);
             break;
         default:
-            result = postEarthquake();
+            result = postEarthquakeMessage();
             break;
         }
         return result;
     }
-    AzureaUtil.mixin(AzureaVim.commands_list, {
-        earthquake: 'earthquake',
-        e: 'earthquake'
-    });
 } catch (err) {}
 
 })();
