f80385bb2137fded0481ad1afb87f5e65eea5ade
ne-sachirou <utakata.c4se@gmail.com>
2011-03-29 07:33:02 +0900

    :translate integration for apiLevel >= 14.

--- src/plugins/translate.js
+++ src/plugins/translate.js
@@ -9,21 +9,25 @@ AzureaUtil.mixin(AzureaVim.commands_list, {
 // option1がを省略した場合、jaへ翻訳します。
 
 AzureaVim.prototype.translate = function() {
+    var view = System.views.currentView,
+        item = view.getItem(view.selectedItemId);
+    
+    function callback(response) { // @param HttpResponce Object:
+        if (response.statusCode !== 200) {
+            throw Error('Google Translate API Error. statusCode is ' + response.statusCode + '.');
+        }
+        item.text = response.body.match(/"translatedText"\s*:\s*"(.*)"/)[1];
+        //response.body.match(/"detectedSourceLanguage"\s*:\s*"(.*)"/)[1] + '-> ' + this.command[1]
+    }
+    
     if (!this.command[1]) {
         this.command[1] = 'ja';
     }
     Http.sendRequestAsync('https://www.googleapis.com/language/translate/v2?key=' +
-                                     'AIzaSyCva1yUIFIBRqXOZDJ0nrbGbm0bz3FIksc' +
-                                     '&q=' +
-                                     encodeURI(this.status_text) +
-                                     '&target=' + this.command[1],
-                                     false,
-                                     function(response) { // @param HttpResponce Object:
-        if (response.statusCode !== 200) {
-            throw Error('Google Translate API Error. statusCode is ' + response.statusCode + '.');
-        }
-        System.showMessage(response.body.match(/"translatedText"\s*:\s*"(.*)"/)[1],
-                           response.body.match(/"detectedSourceLanguage"\s*:\s*"(.*)"/)[1] + '-> ',// + this.command[1],
-                           0);
-    });
+                          'AIzaSyCva1yUIFIBRqXOZDJ0nrbGbm0bz3FIksc' +
+                          '&q=' +
+                          encodeURI(this.status_text) +
+                          '&target=' + this.command[1],
+                          false,
+                          callback);
 }
